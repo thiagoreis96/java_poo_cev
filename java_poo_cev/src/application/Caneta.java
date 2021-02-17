0e291ba82a852fe354b38b53f47cf8632e9b0eab
thiagoreis96 <thiago.reis96@icloud.com>
2021-02-17 16:27:31 -0300

    Enviando Program 3, da aula 4

--- java_poo_cev/src/application/Caneta.java
+++ java_poo_cev/src/application/Caneta.java
@@ -1,32 +1,47 @@
 package application;
 
 public class Caneta {
-	public String modelo;
-	public String cor;
-	public double ponta;
-	public int carga;
-	public boolean tampada;
-	public void status() {
-		System.out.println("Modelo: " + this.modelo);
-		System.out.println("Uma caneta " + this.cor);
-		System.out.println("Ponta: " + this.ponta);
-		System.out.println("Carga: " + this.carga);
-		System.out.println("Está tampada? " + this.tampada);
+	private String modelo;
+	private String cor;
+	private double ponta;
+	private boolean tampada;
+	
+	public Caneta(String modelo, String cor, double ponta) {
+		this.modelo = modelo;
+		this.cor = cor;
+		this.ponta = ponta;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public double getPonta() {
+		return ponta;
+	}
+
+	public void setPonta(double ponta) {
+		this.ponta = ponta;
+	}
+
+	public boolean isTampada() {
+		return tampada;
+	}
+
+	public void setTampada(boolean tampada) {
+		this.tampada = tampada;
 	}
 	
-	public void rabiscar() {
-		if (this.tampada == true) {
-			System.out.println("ERRO! Não posso rabiscar");
-		} else {
-			System.out.println("Estou rabiscando");
-		}
-	}
-	
-	public void tampar () {
-		this.tampada = true;
-	}
-	
-	public void destampar() {
-		this.tampada = false;
-	}
 }
--- java_poo_cev/src/application/Program1.java
+++ java_poo_cev/src/application/Program1.java
@@ -1,4 +1,4 @@
-package application;
+/*package application;
 
 public class Program1 {
 
@@ -21,3 +21,4 @@ public class Program1 {
 		c2.rabiscar();
 	}
 }
+*/
