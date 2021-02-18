09b5e761763b790373fb7eca2584cba90c19ca9b
thiagoreis96 <thiago.reis96@icloud.com>
2021-02-17 22:15:24 -0300

    Enviando as classes e a interface da aula 6

---
+++ java_poo_cev/src/application/Program5.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		
+		ControleRemoto c = new ControleRemoto();
+		c.maisVolume();
+		c.play();
+		c.abrirMenu();
+		c.fecharMenu();
+
+	}
+
+}
