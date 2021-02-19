199d5eea79c1fdffbd69fa17ebd53dbcc7ab0ab1
thiagoreis96 <thiago.reis96@icloud.com>
2021-02-19 01:05:58 -0300

    Adicionando classes da aula 13

---
+++ java_poo_cev/src/exercises/Ex04.java
@@ -0,0 +1,16 @@
+package exercises;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		
+		Cachorro c = new Cachorro();
+		
+		c.reagir(true);
+		c.reagir(false);
+		
+		
+
+	}
+
+}
---
+++ java_poo_cev/src/exercises/Lobo.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Lobo extends Mamifero {
+	
+	@Override
+	public void emitirSom() {
+		System.out.println("Auuuuuuuuuuu!");
+	}
+}
