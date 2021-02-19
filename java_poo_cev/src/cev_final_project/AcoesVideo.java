405bed36a882efec026d095e43dc863194d76c33
thiagoreis96 <thiago.reis96@icloud.com>
2021-02-19 10:43:20 -0300

    Enviando o pacote, a interface e as classes do projeto final do curso

---
+++ java_poo_cev/src/cev_final_project/AcoesVideo.java
@@ -0,0 +1,7 @@
+package cev_final_project;
+
+public interface AcoesVideo {
+	public void play();
+	public void pause();
+	public void like();
+}
---
+++ java_poo_cev/src/cev_final_project/Gafanhoto.java
@@ -0,0 +1,32 @@
+package cev_final_project;
+
+public class Gafanhoto extends Pessoa {
+	private String login;
+	private int totAssistido;
+	
+	public Gafanhoto(String name, int idade, String sexo, String login) {
+		super(name, idade, sexo);
+		this.login = login;
+		this.totAssistido = 0;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public int getTotAssistido() {
+		return totAssistido;
+	}
+	public void setTotAssistido(int totAssistido) {
+		this.totAssistido = totAssistido;
+	}
+
+	@Override
+	public String toString() {
+		return "Gafanhoto {" + super.toString() + "\nlogin=" + login + ", totAssistido=" + totAssistido + "}";
+	}
+	
+	
+}
