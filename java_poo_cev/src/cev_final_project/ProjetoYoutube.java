b10b43f836db2ac71cfe8bcc1c44fb09b5dc9f2d
thiagoreis96 <thiago.reis96@icloud.com>
2021-02-19 12:17:46 -0300

    Enviando o desafio final completo

--- java_poo_cev/src/cev_final_project/ProjetoYoutube.java
+++ java_poo_cev/src/cev_final_project/ProjetoYoutube.java
@@ -12,10 +12,14 @@ public class ProjetoYoutube {
 		Gafanhoto[] g = new Gafanhoto[2];
 		g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
 		g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
+
+		Visualizacao[] vis = new Visualizacao[5];
+		vis[0] = new Visualizacao(g[0], v[2]);
+		vis[0].avaliar();
+		System.out.println(vis[0].toString());
 		
-		System.out.println(v[0].toString());
-		System.out.println(g[0].toString());
-
+		vis[1] = new Visualizacao(g[0], v[1]);
+		vis[0].avaliar(87.0);
+		System.out.println(vis[1].toString());
 	}
-
 }
